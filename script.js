922f2936a67800d0d0f9bf9f885aebe4599e848d
fuzzyhax1384 <107611430+fuzzyhax1384@users.noreply.github.com>
2023-02-08 21:22:43 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector("#new-arrivals-button").addEventListener("click", function() {
+    alert("You will be redirected to the New Arrivals section of the Roblox Clothing Store.");
+  });
+  
+  document.querySelector("#best-sellers-button").addEventListener("click", function() {
+    alert("You will be redirected to the Best Sellers section of the Roblox Clothing Store.");
+  });
+  
+  document.querySelector("#sale-button").addEventListener("click", function() {
+    alert("You will be redirected to the Sale section of the Roblox Clothing Store.");
+  });
+  
